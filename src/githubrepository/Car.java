590be172b3022cfa42fd0f1853037c8ac9fc1b2a
Pedro Vinicius <peuvini.94@gmail.com>
2023-12-02 22:11:36 +0000

    Create Car.java

---
+++ src/githubrepository/Car.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package githubrepository;
+
+/**
+ *
+ * @author peuvi
+ */
+public class Car {
+    
+     String Make;
+    String Model;
+    String Colour;
+    int NumWheels;
+
+    public Car(String Make, String Model, String Colour, int NumWheels) {
+        this.Make = Make;
+        this.Model = Model;
+        this.Colour = Colour;
+        this.NumWheels = NumWheels;
+    }
+    
+}
