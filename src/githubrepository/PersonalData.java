e69d7d9a5955106c9f9ab8d78cf582f0e4b7bb53
Pedro Vinicius <peuvini.94@gmail.com>
2023-12-02 22:47:34 +0000

    Variables added
    Strings "Name," "Address," "Email," and integer "Contact Number" have been added.

---
+++ src/githubrepository/PersonalData.java
@@ -0,0 +1,19 @@
+   
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package githubhomework;
+
+/**
+ *
+ * @author peuvi
+ */
+public class PersonalData {
+    String Name;
+    String Address;
+    String Email;
+    int ContactNumber;
+
+
+}
